57c0c9a335a25bf69c6d75b4c6d84a38e335b51c
wsaicyj <wsaicyj@gmail.com>
2016-05-31 18:50:04 +0800

    sample

--- hello.py
+++ hello.py
@@ -4,23 +4,44 @@
 __author__ = 'Aaron_chan'
 
 
-from flask import Flask
+from flask import Flask,render_template
 from flask import request
 from flask import make_response
 from flask import abort
-from flask.ext.script import Manager
+#from flask.ext.script import Manager
+from flask_script import Manager
+#from flask.ext.bookstrap import Bootstrap
+from flask_bootstrap import Bootstrap
+
 
 app = Flask(__name__)
-manager = Manager(app)
-@app.route('/')
-def index():
-    user_agent = request.headers.get('User-Agent')
-    return '<h1>Your browser is %s!</h1>' % user_agent
-    #return '<h1>hello,Flask</h1>'
+#manager = Manager(app)
+bookstarp = Bootstrap(app)
+
+@app.errorhandler(400)
+def page_not_found():
+    return render_template('404'),404
+
+@app.errorhandler(500)
+def internal_server_error():
+    return render_template('500'),500
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>hello,%s!</h1>' % name
+    return render_template('user.html',name=name)
+
+'''
+@app.route('/')
+def index():
+    #user_agent = request.headers.get('User-Agent')
+    #return '<h1>Your browser is %s!</h1>' % user_agent
+    #return '<h1>hello,Flask</h1>'
+    return render_template('index.html')
+
+@app.route('/user/<name>')
+def user(name):
+    #return '<h1>hello,%s!</h1>' % name
+    return render_template('user.html',name=name)
 
 @app.route('/response')
 def cook():
@@ -30,12 +51,12 @@ def cook():
 
 @app.route('/user1/<id>')
 def get_user(id):
-    user = load_user(id)
+    #user = load_user(id)
     if not user:
         abort(404)
     return '<h1>Hello,%s!</h1>' % user.name
-
+'''
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    manager.run()
+    app.run(debug=True)
+    #manager.run()
