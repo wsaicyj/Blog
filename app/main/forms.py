091022529bcabc1d278142d7d3753c6cfbc5503f
wsaicyj <wsaicyj@gmail.com>
2016-07-05 00:31:17 +0800

    用户资料1

--- app/main/forms.py
+++ app/main/forms.py
@@ -4,8 +4,8 @@
 __author__ = 'Aaron_chan'
 
 from flask_wtf import Form
-from wtforms import StringField,SubmitField
-from wtforms.validators import Required
+from wtforms import StringField,SubmitField,TextAreaField
+from wtforms.validators import Required,Length
 
 class NameForm(Form):
     '''
@@ -13,4 +13,13 @@ class NameForm(Form):
     '''
 
     name = StringField('What is your name?',validators=[Required()])
+    submit = SubmitField('Submit')
+
+
+
+class EditProfileForm(Form):
+    '''用户级别的资料编辑器'''
+    name = StringField('Real name',validators=[Length(0,64)])
+    location = StringField('Location',validators=[Length(0,64)])
+    about_me = TextAreaField('About me')
     submit = SubmitField('Submit')
--- app/main/views.py
+++ app/main/views.py
@@ -6,11 +6,13 @@ __author__ = 'Aaron_chan'
 #保存程序的路由
 
 from datetime import datetime
-from flask import render_template,session,redirect,url_for,abort
+from flask import render_template,session,redirect,url_for,abort,flash
 from . import main
 from .forms import NameForm
 from .. import db
 from ..models import User
+from flask_login import login_required,current_user
+from .forms import EditProfileForm
 
 '''
 @main.route('/',methods=['GET','POST'])
@@ -32,3 +34,20 @@ def user(username):
     if user is None:
         abort(404)
     return render_template('user.html',user=user)
+
+@main.route('/edit_profile',methods=['GET','POST'])
+@login_required
+def edit_profile():
+    '''资料编辑路由'''
+    form = EditProfileForm()
+    if form.validate_on_submit():
+        current_user.name = form.name.data
+        current_user.location = form.location.data
+        current_user.about_me = form.about_me.data
+        db.session.add(current_user)
+        flash('Your profile has been updated.')
+        return redirect(url_for('.user',username=current_user.username))
+    form.name.data = current_user.name
+    form.location.data = current_user.location
+    form.about_me.data = current_user.about_me
+    return render_template('edit_profile.html',form=form)
